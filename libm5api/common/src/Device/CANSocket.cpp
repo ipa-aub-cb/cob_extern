1058ea9e2963f57a9c771c89ce9f4a8b95915183
ipa-aub-cb <christianbuesch@gmx.net>
2011-12-22 15:31:19 +0100

    not working, with comments, removed unnecessary code

--- libm5api/common/src/Device/CANSocket.cpp
+++ libm5api/common/src/Device/CANSocket.cpp
@@ -14,20 +14,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// #define DEBUG 1
+
 //#include "Neobotix/Compat.h"
 
 
 struct can_frame frame;
 
 //-----------------------------------------------
-CANSocket::CANSocket()//const char* cIniFile)
+CANSocket::CANSocket() : m_hDevice(0), m_hSyncDevice(0), m_iDeviceId(-1), m_uiBaudRate(0), m_uiQueueSize(128), m_uiTimeOut(3)		// ESD C331 //const char* cIniFile)
 {
+	initMessage("CANSocket", g_iDebugLevel, g_bDebug, g_bDebugFile);
+	m_DeviceName = (char*) malloc(200 * sizeof(char));
+	memset(m_DeviceName,0,sizeof(m_DeviceName));
 	m_bInitialized = false;
 
-	// read IniFile
-	//_IniFile.SetFileName(cIniFile, "CANSocket.cpp");
-
-	init();
 }
 
 //-----------------------------------------------
@@ -38,13 +39,17 @@ CANSocket::~CANSocket()
 		//CAN_Close(m_handle);
 	}
 }
-
-//-----------------------------------------------
-void CANSocket::init()
+int CANSocket::init()
 {
-	//m_handle = LINUX_CAN_Open("/dev/pcan32", O_RDWR | O_NONBLOCK);
-	printf("Trying to open CAN on /dev/can0 ...\n");
-    //m_handle = LINUX_CAN_Open("/dev/pcan32", O_RDWR);
+return init(0x014);//CAN_BAUD_250K);
+}
+//-----------------------------------------------
+int CANSocket::init(const char* acInitString)
+{
+#ifdef DEBUG
+	std::cout << "Trying to open CAN on can0 ...\n"<< std::endl;
+#endif
+	
 	m_skt = socket( PF_CAN, SOCK_RAW, CAN_RAW );
 	struct ifreq ifr;
 	strcpy(ifr.ifr_name, "can0");
@@ -53,66 +58,124 @@ void CANSocket::init()
 	addr.can_family = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
 	int ret1 = bind( m_skt, (struct sockaddr*)&addr, sizeof(addr) );
+	m_iErrorState = 0;
+	m_bInitFlag = true;
+
+	updateModuleIdMap();
+
+#ifdef DEBUG
+	std::cout << "finished updateModuleIdMap "<< std::endl;
+#endif
 	
 	if (! m_skt)
 	{
 		// Fatal error
-		std::cout << "Cannot open CAN on USB: " << strerror(errno);
-		//Sleep(3000);
-		//exit(0);
+		std::cout << "Cannot open CAN on USB: " << strerror(errno)<< std::endl;
 	}
         else 
-          {printf("success!\n");
-		m_bInitialized = true;}
-	
+          {
+			printf("Open CAN on USB suceeded!\n");
+			m_bInitialized = true;
+		  }
+		  
+#ifdef DEBUG
+	std::cout << "m_iErrorState before returning of CANSocket::init(const char* acInitString): "<< m_iErrorState;	
+#endif
+
+	return m_iErrorState;
 }
 
 //-------------------------------------------
-bool CANSocket::transmitMsg(CanMsg CMsg, bool bBlocking)
-{
-	int bytes_sent =1;
-	if (m_bInitialized == false) return false;
-	//char data1[8];
-	//struct can_frame frame;
-	frame.can_id = CMsg.m_iID;
-	//std::cout << "frame id is " << frame.can_id << std::endl;
-	
-	for(int i=0; i<8; i++)
-	{
-		frame.data[i] = CMsg.getAt(i);
-		//std::cout << "Msg: " << CMsg.getAt(i) << std::endl;
-	}
 
+int CANSocket::init(unsigned long baudRate)
+{
+	return m_iErrorState;
+}
+
+//-------------------------------------------
+int CANSocket::writeDevice(CProtocolMessage& rclProtocolMessage)
+{
+#ifdef DEBUG
+	std::cout << "CANSocket::writeDevice "<< std::endl;
+#endif
 	
-	frame.can_dlc = CMsg.m_iLen;
-	//frame.can_type = CMsg.m_iType;
-	bool bRet = true;
-	if (bBlocking)
-		bytes_sent = write( m_skt, &frame, sizeof(frame) );
-		//std::cout << "bblocking bytes sent " << bytes_sent << std::endl;}
-	else
-		bytes_sent = write( m_skt, &frame, sizeof(frame) );
-		//std::cout << "not bblocking bytes sent " << bytes_sent << std::endl;}
+	int bytes_sent =-1;
+#ifdef DEBUG
+	std::cout << "m_skt " << m_skt << std::endl;
+#endif	
+	if (m_bInitialized == false) return false;
+	
+	frame.can_dlc = (int)rclProtocolMessage.m_ucMessageLength;
+	
+#ifdef DEBUG
+	std::cout << "frame can_ldc is " << frame.can_dlc << std::endl;
+	std::cout << "clProtocolMessage.m_ucMessageLength: " << (unsigned int)rclProtocolMessage.m_ucMessageLength << std::endl;
+#endif	
+
+	frame.can_id = rclProtocolMessage.m_uiMessageId;
+
+#ifdef DEBUG
+	std::cout << "frame can_id is " << frame.can_id << std::endl;
+	std::cout << "rclProtocolMessage.m_uiMessageId: " << rclProtocolMessage.m_uiMessageId << std::endl;
+#endif
+	
+	// copy data from ProtocolMessage to frame.data (byte-wise or string)
+	
+	//memcpy(frame.data, "", 2);
+	//memcpy(frame.data, rclProtocolMessage.m_aucMessageData, rclProtocolMessage.m_ucMessageLength);
+	
+	for(int i=0; i<(unsigned int)rclProtocolMessage.m_ucMessageLength; i++)
+	{
+		
+		frame.data[i] = rclProtocolMessage.m_aucMessageData[i];
+		#ifdef DEBUG
+			std::cout << "rclProtocolMessage.m_aucMessageData[i] " << rclProtocolMessage.m_aucMessageData[i] << std::endl;
+			std::cout << "frame data is " << frame.data << std::endl;
+			std::cout << "rclProtocolMessage.m_aucMessageData: " << rclProtocolMessage.m_aucMessageData << std::endl;
+		#endif
+	}
+#ifdef DEBUG	
+	std::cout << "sizeof frame : " << sizeof(frame) << std::endl;
+	std::cout << "start write:" << std::endl;
+#endif
+	
+	int bRet = 1;
+	
+	//writing
+	bytes_sent = write( m_skt, &frame, sizeof(frame));
 
 	if( bytes_sent < 0)
 	{
-		std::cout << "error in CanSocket::transmitMsg: " << std::endl;//GetErrorStr(ret) << std::endl;
-		bRet = false;
+	#ifdef DEBUG
+		std::cout << "error in CanSocket::transmitMsg: bytes sent <0" << std::endl;
+	#endif
+		bRet = 1;
 	}
 
-	m_LastID = frame.data[0];
+	
+	if ( bytes_sent > 0) 
+	{
+	bRet =0;
+#ifdef DEBUG
+	std::cout << "bytes gesendet in CanSocket::transmitMsg:  " << bytes_sent << std::endl;
+#endif
+}
 
-	if (bytes_sent == 0) 
-		//m_bIsTXError = true;
-	return !m_bIsTXError;
+#ifdef DEBUG
+	std::cout << "bRet: " << bRet << std::endl;
+#endif
 
 return bRet;
 
 }
 
 //-------------------------------------------
-bool CANSocket::receiveMsg(CanMsg* pCMsg)
+
+
+int CANSocket::readDevice(CProtocolMessage& rclProtocolMessage)
 {
+	// set frame to zero
+	
 	frame.data[0] = 0;
 	frame.data[1] = 0;
 	frame.data[2] = 0;
@@ -121,67 +184,66 @@ bool CANSocket::receiveMsg(CanMsg* pCMsg)
 	frame.data[5] = 0;
 	frame.data[6] = 0;
 	frame.data[7] = 0;
-	//frame.msg_lost = 0;
-	frame.can_id = 0;
-	frame.can_dlc = 0;
-	//int iRet = CAN_ERR_OK;
-	bool bRet = false;
-	if (m_bInitialized == false) return false;
+	frame.can_dlc = 8;	//rclProtocolMessage.m_ucMessageLength;
+	frame.can_id = 0; //rclProtocolMessage.m_uiMessageId;
 	
+	int bRet = 1;
+	
+	//read
 	int bytes_read = read( m_skt, &frame, sizeof(frame) );
+	
+#ifdef DEBUG
+	std::cerr << "sizeof(frame)" << sizeof(frame) << std::endl;
+#endif
+	
 	
 	if( !isObjectMode() ) 
 	{
-		if (bytes_read > 0) 
+		if (bytes_read > 0)  // no problem occured
 		{
-			// message received
-			pCMsg->m_iID = frame.can_id;
-			//pCMsg->m_iLen = NTCANMsg.len;
-			pCMsg->set(frame.data[0], frame.data[1], frame.data[2], frame.data[3],
-				frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
-			bRet = true;
+			
+			#ifdef DEBUG
+				std::cout << "bytes read: " << bytes_read << std::endl;
+				std::cout << "data: " << frame.data << std::endl;
+				std::cout << "ID: " << frame.can_id << std::endl;
+				std::cout << "length: " << frame.can_dlc << std::endl;
+			#endif
+		//  copy read data from frame to ProtocolMessage (byte-wise or string)
+		
+		//	frame.data[1], frame.data[2], frame.data[3],frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
+			
+			rclProtocolMessage.m_uiMessageId = frame.can_id;
+			rclProtocolMessage.m_ucMessageLength = frame.can_dlc;
+			memcpy(rclProtocolMessage.m_aucMessageData, frame.data, rclProtocolMessage.m_ucMessageLength);
+						
+			bRet = 0;
 		}
-		else
+		else 	// reading problem occured
 		{
 			// no message, errror
 			std::cout << "error in CanSocket::receiveMsg: " << std::endl;//GetErrorStr(ret) << std::endl;
-				
-			pCMsg->m_iID = frame.can_id;
-			pCMsg->set(0,0,0,0,0,0,0,0);
-	
-//			std::cout << "len=" << len << "; ret = " << GetErrorStr(ret) << std::endl;
-			bRet = false;
+			bRet = 1;
 		}
 	}
-	else 
-	{
-		if (bytes_read > 0) 
-		{
-			// message received
-			pCMsg->m_iID = frame.can_id;
-			//pCMsg->m_iLen = NTCANMsg.len;
-			pCMsg->set(frame.data[0], frame.data[1], frame.data[2], frame.data[3],
-				frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
-			bRet = true;
-		}
-		else
-		{
-			// no message, errror
-			std::cout << "error in CanSocket::receiveMsg: " << std::endl;//GetErrorStr(ret) << std::endl;		
-			pCMsg->m_iID = frame.can_id;
-			pCMsg->set(0,0,0,0,0,0,0,0);
-//			std::cout << "len=" << len << "; ret = " << GetErrorStr(ret) << std::endl;
-			bRet = false;
-		}
-	}
-	
+
+#ifdef DEBUG
+	std::cout << "bRet before return from CANSocket::readDevice: " << bRet << std::endl;
+#endif	
 	return bRet;
-	
+
+}
+
+
+int CANSocket::reinit(unsigned char ucBaudRateId)
+{
+	return m_iErrorState;
 }
 
 //-------------------------------------------
+
 bool CANSocket::receiveMsgRetry(CanMsg* pCMsg, int iNrOfRetry)
 {
+	can_frame frame;
 	int i, iRet;
 
 	frame.can_id = 0;
@@ -224,3 +286,61 @@ bool CANSocket::receiveMsgRetry(CanMsg* pCMsg, int iNrOfRetry)
 	return bRet;
 }
 
+
+int CANSocket::exit()
+{
+	return m_iErrorState;
+}
+
+
+int CANSocket::waitForStartMotionAll()
+{
+	return m_iErrorState;
+}
+
+
+void CANSocket::setQueueSize(unsigned short uiQueueSize)
+{
+	m_uiQueueSize = uiQueueSize;
+}
+
+
+void CANSocket::setTimeOut(unsigned long uiTimeOut)
+{
+	m_uiTimeOut= uiTimeOut;
+}
+
+int CANSocket::getDeviceError(int iErrorState)
+{
+	return m_iErrorState;
+}
+
+
+int CANSocket::setBaudRate(unsigned char iBaudRate)
+{
+	m_uiBaudRate = (unsigned long) iBaudRate;
+	return setBaudRate();
+}
+
+
+int CANSocket::setBaudRate()
+{
+
+	return m_iErrorState;
+}
+
+
+int CANSocket::setMessageId(unsigned long uiMessageId)
+{
+
+	return m_iErrorState;
+}
+
+
+int CANSocket::clearReadQueue()
+{
+	can_frame frame;
+    frame.can_dlc = 8;
+	frame.can_id = 0;
+	return m_iErrorState;
+}
